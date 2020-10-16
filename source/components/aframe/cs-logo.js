7145b2f83f581cc80ce7ca8d2061b4167f427d87
Dan Buckland <danbucklan@gmail.com>
2020-10-16 12:40:16 +0100

    Spawn cs-logo instead

--- source/components/aframe/cs-logo.js
+++ source/components/aframe/cs-logo.js
@@ -1,3 +1,5 @@
+import './velocity-glow';
+
 AFRAME.registerComponent('cs-logo', {
   multiple: true,
   
@@ -7,11 +9,12 @@ AFRAME.registerComponent('cs-logo', {
     let csLogoArms = document.createElement('a-entity');
     csLogoArms.setAttribute('gltf-model', '#cs-logo');
     csLogoArms.setAttribute('position', '0 1.5 -1');
-    csLogoArms.setAttribute('dynamic-body', { shape: 'none', mass: 0.5, angluarDamping: 0.5, linearDamping: 0.9 });
+    csLogoArms.setAttribute('dynamic-body', { shape: 'none', mass: 0.5, angularDamping: 0.5, linearDamping: 0.9 });
     csLogoArms.setAttribute('shape__top', { shape: 'box', halfExtents: '0.25 0.04455 0.04455', offset: '0 0.20545 0' });
     csLogoArms.setAttribute('shape__right', { shape: 'box', halfExtents: '0.04455 0.25 0.04455', offset: '0.20545 0 0' });
     csLogoArms.setAttribute('shape__bottom', { shape: 'box', halfExtents: '0.1475 0.04455 0.04455', offset: '0.1 -0.20545 0' });
     csLogoArms.setAttribute('shape__left', { shape: 'box', halfExtents: '0.04455 0.1475 0.04455', offset: '-0.20545 0.1  0' });
+    csLogoArms.setAttribute('velocity-glow', '');
 
     let csLogoDot = document.createElement('a-cylinder');
     csLogoDot.setAttribute('color', '#212121');
@@ -20,11 +23,11 @@ AFRAME.registerComponent('cs-logo', {
     csLogoDot.setAttribute('dynamic-body', { mass: 0.1, angularDamping: 0.5, linearDamping: 0.9 });
     csLogoDot.setAttribute('position', '-0.1917 1.3083 -1');
     csLogoDot.setAttribute('rotation', '90 0 0');
+    csLogoDot.setAttribute('velocity-glow', '');
     
     csLogo.appendChild(csLogoArms);
     csLogo.appendChild(csLogoDot);
 
     this.el.appendChild(csLogo);
-    
   },
 });
--- source/components/client.js
+++ source/components/client.js
@@ -9,16 +9,10 @@ AFRAME.registerComponent('client', {
     let socket = io();
 
     socket.on('spawnEntity', function (data) {
-      let randomRadius = 0.05 + Math.random() * 0.1;
-      let spawn = document.createElement('a-sphere');
-      spawn.setAttribute('radius', randomRadius);
-      spawn.setAttribute('position', '0 2 -1')
-      spawn.setAttribute('material', 'color', '#232323');
-      spawn.setAttribute('dynamic-body', { mass: randomRadius, linearDamping: 0.5, angularDamping: 0.5 });
-      spawn.setAttribute('velocity-glow', '');
-      spawn.setAttribute('grab', '');
-      spawn.setAttribute('class', 'collidable');
-      spawn.setAttribute(`destroyer`, { ttl: 120 } );
+      let spawn = document.createElement('a-entity');
+      spawn.setAttribute('cs-logo', '');
+      spawn.setAttribute('position', '0 0 0');
+      spawn.setAttribute('destroyer', { ttl: 120 } );
       scene.appendChild(spawn);
     });
   },
