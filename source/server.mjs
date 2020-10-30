1b63af9f531aee3b61aa0bce159c834ed9377980
Dan Buckland <danbucklan@gmail.com>
2020-10-30 12:00:57 +0000

    Set server code to be production ready

--- source/server.mjs
+++ source/server.mjs
@@ -9,19 +9,31 @@ import fs from 'fs';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import config from '../webpack.config.js';
 
-const compiler = webpack(config);
+// const compiler = webpack(config);
 const app = express();
 
-app.use(webpackDevMiddleware(compiler, {
-	publicPath: config.output.publicPath,
-}));
+app.use(express.static('public'));
+// TODO: Add this to local config
+// app.use(webpackDevMiddleware(compiler, {
+// 	publicPath: config.output.publicPath,
+// }));
 
 app.use('/assets/', express.static(config.output.path + '/assets/'));
 
 // start HTTPS server listening on port 2002
+// TODO: Enable both prod and local environments
+// const server = https.createServer({
+// 	key: fs.readFileSync('localhost-key.pem'),
+// 	cert: fs.readFileSync('localhost.pem'),
+// 	requestCert: false,
+// 	rejectUnauthorized: false
+// }, app).listen(2002, () => {
+// 	console.log('listening on *:2002')
+// });
+
 const server = https.createServer({
-	// key: fs.readFileSync('localhost-key.pem'),
-	// cert: fs.readFileSync('localhost.pem'),
+	key: fs.readFileSync('localhost-key.pem'),
+	cert: fs.readFileSync('localhost.pem'),
 	requestCert: false,
 	rejectUnauthorized: false
 }, app).listen(2002, () => {
