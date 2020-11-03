d42a4aa754a12244b620b86bf5cf56108da53839
Dan Buckland <danbucklan@gmail.com>
2020-11-03 17:37:09 +0000

    Remove unused files

--- source/app.js
+++ source/app.js
@@ -1,8 +1,6 @@
-import './styles.css';
 import 'aframe';
 
 import './components/aframe/cs-logo';
-import './components/aframe/client';
 import './components/aframe/game';
 
 window.CLOCK = new THREE.Clock();
