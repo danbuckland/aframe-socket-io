7dece0a6d23f3668cc9d794717202fa9d562a562
Dan Buckland <danbucklan@gmail.com>
2020-11-05 15:53:03 +0000

    Use named functions and closures in game tick handler

--- source/components/aframe/game.js
+++ source/components/aframe/game.js
@@ -3,13 +3,15 @@ let socket;
 let localPlayerId;
 const localIds = [];
 let remoteData = [];
+let scene;
 
 // TODO: Consider turning game into a system https://aframe.io/docs/1.0.0/core/systems.html
 AFRAME.registerComponent('game', {
 
 	init: function () {
 		// append new local-player component to scene
-		this.el.sceneEl.setAttribute('local-player', '');
+		scene = this.el.sceneEl;
+		scene.setAttribute('local-player', '');
 
 		socket.on('remoteData', function (data) { remoteData = data });
 
@@ -34,67 +36,83 @@ AFRAME.registerComponent('game', {
 	},
 
 	tick: function (t, dt) {
-		// TODO: Use closures to avoid creating new variables each frame - 
-		// https://aframe.io/docs/1.0.0/introduction/best-practices.html#tick-handlers
-		let scene = this.el.sceneEl;
 		// Return if there are no remote players
 		if (remoteData === undefined || remoteData.length == 0 || localPlayerId === undefined) { return };
 
-		let remoteIds = remoteData.map(player => player.id);
-		remoteData.forEach(function (data) {
-			if (localPlayerId != data.id) {
-				if (!document.getElementById(data.id)) {
-					// Append player to scene if remote player does not exist
-					// TODO: Check for race conditions and consider implementing concept of initialising players
-					// TODO: Add a create remote player function or constructor
-					let remotePlayer = document.createElement('a-entity');
-					remotePlayer.setAttribute('player', {
-						id: data.id,
-						shape: data.shape,
-						color: data.color,
-						position: data.position
-					});
-					scene.appendChild(remotePlayer);
-					localIds.push(data.id);
-				} else if (document.getElementById(data.id)) {
-					// Update remote player position if it does exist
-					let remotePlayer = document.getElementById(data.id);
-					remotePlayer.object3D.position.copy(data.position);
-					remotePlayer.object3D.quaternion.set(data.rx, data.ry, data.rz, data.rw);
-				}
-			}
-		});
-		// After creating any missing remote player locally, delete local player not on the remote
-		if (JSON.stringify(remoteIds.sort()) !== JSON.stringify(localIds.sort())) { // discrepancy exists
-			let orphanedIds = localIds.filter(x => !remoteIds.includes(x));
-			orphanedIds.forEach(function (id) {
-				if (id != localPlayerId ) {
-					console.log(`Deleting orphan ${id}`);
-					let orphanedElement = document.getElementById(id);
-					orphanedElement.parentNode.removeChild(orphanedElement);
-					let i = localIds.indexOf(id);
-					localIds.splice(i, i + 1);
-				}
-			})
-		};
+		this.updatePlayersInScene();
+		this.removeLocalOrphans();
+		this.updateLocalPlayerOnServer();
+	},
 
-		var position = new THREE.Vector3();
-		var quaternion = new THREE.Quaternion();
-
-		let localPlayerElement = document.getElementById(localPlayerId);
-		if (localPlayerElement) {
-			localPlayerElement.object3D.getWorldPosition(position);
-			localPlayerElement.object3D.getWorldQuaternion(quaternion);
+	updatePlayersInScene: (function () {
+		return function () {
+			remoteData.forEach(function (data) {
+				if (localPlayerId != data.id) {
+					if (!document.getElementById(data.id)) {
+						// Append player to scene if remote player does not exist
+						// TODO: Check for race conditions and consider implementing concept of initialising players
+						// TODO: Add a create remote player function or constructor
+						let remotePlayer = document.createElement('a-entity');
+						remotePlayer.setAttribute('player', {
+							id: data.id,
+							shape: data.shape,
+							color: data.color,
+							position: data.position
+						});
+						scene.appendChild(remotePlayer);
+						localIds.push(data.id);
+					} else if (document.getElementById(data.id)) {
+						// Update remote player position if it does exist
+						let remotePlayer = document.getElementById(data.id);
+						remotePlayer.object3D.position.copy(data.position);
+						remotePlayer.object3D.quaternion.set(data.rx, data.ry, data.rz, data.rw);
+					}
+				}
+			});
 		}
+	})(),
 
-		socket.emit('update', {
-			position: position,
-			rx: quaternion.x,
-			ry: quaternion.y,
-			rz: quaternion.z,
-			rw: quaternion.w
-		});
-	}
+	removeLocalOrphans: (function () {
+		return function () {
+			// After creating any missing remote player locally, delete local player not on the remote
+			let remoteIds = remoteData.map(player => player.id);
+			if (JSON.stringify(remoteIds.sort()) !== JSON.stringify(localIds.sort())) { // discrepancy exists
+				let orphanedIds = localIds.filter(x => !remoteIds.includes(x));
+				orphanedIds.forEach(function (id) {
+					if (id != localPlayerId) {
+						console.log(`Deleting orphan ${id}`);
+						let orphanedElement = document.getElementById(id);
+						orphanedElement.parentNode.removeChild(orphanedElement);
+						let i = localIds.indexOf(id);
+						localIds.splice(i, i + 1);
+					}
+				})
+			};
+		}
+	})(),
+
+	updateLocalPlayerOnServer: (function () {
+		// https://aframe.io/docs/1.0.0/introduction/best-practices.html#tick-handlers
+		let position = new THREE.Vector3();
+		let quaternion = new THREE.Quaternion();
+
+		return function () {
+			let localPlayerElement = document.getElementById(localPlayerId);
+			if (localPlayerElement) {
+				localPlayerElement.object3D.getWorldPosition(position);
+				localPlayerElement.object3D.getWorldQuaternion(quaternion);
+			}
+
+			socket.emit('update', {
+				position: position,
+				rx: quaternion.x,
+				ry: quaternion.y,
+				rz: quaternion.z,
+				rw: quaternion.w
+			});
+		}
+	})()
+
 });
 
 // builds the player model itself
@@ -119,13 +137,13 @@ AFRAME.registerComponent('player', {
 		let shape = this.data.shape;
 		let local = !shape;
 		let position = this.data.position;
-		
+
 		// if player component is local, set some values for the player
 		if (local) {
 			color = colors[Math.floor(Math.random() * colors.length)];
 			shape = shapes[Math.floor(Math.random() * shapes.length)];
 		}
-		
+
 		// create the 3d model of the player 
 		el.setAttribute('id', this.data.id);
 		el.setAttribute('geometry', { primitive: shape });
