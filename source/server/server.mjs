2504be1eeedd429ae2b64af821fa53f7116fa0d5
Dan Buckland <danbucklan@gmail.com>
2020-11-11 15:46:05 +0000

    Separate socket events from server creation

--- source/server/server.mjs
+++ source/server/server.mjs
@@ -9,6 +9,7 @@ import sslRedirect from 'heroku-ssl-redirect';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import config from '../../webpack.config.js';
+import sockets from './sockets.js';
 
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -54,40 +55,7 @@ app.get('/', function (req, res) {
 	res.sendFile(publicPath + '/index.html');
 });
 
-// when a client connects, log it on the server and spawn an object for others
-io.on('connection', function (socket) {
-	// define custom user data for position and direction
-	socket.userData = { position: { x: 0, y: 0, z: 0 }, rx: 0, ry: 0, rz: 0, rw: 1 };
-	console.log(`${socket.id} connected`);
-	// emit the socket id to connected clients
-	socket.emit('setId', { id: socket.id });
-
-	// on disconnect, let all other sockets know which socket disconnected
-	socket.on('disconnect', function () {
-		socket.broadcast.emit('deletePlayer', { id: socket.id });
-		console.log(`${socket.id} disconnected`);
-	});
-
-	// when a client initialises, set the data on the socket to match
-	socket.on('init', function (data) {
-		socket.userData.shape = data.shape;
-		socket.userData.color = data.color;
-		socket.userData.position = data.position;
-		socket.userData.rx = data.rx;
-		socket.userData.ry = data.ry;
-		socket.userData.rz = data.rz;
-		socket.userData.rw = data.rw;
-	});
-
-	// update user data that changes frame to frame
-	socket.on('update', function (data) {
-		socket.userData.position = data.position;
-		socket.userData.rx = data.rx;
-		socket.userData.ry = data.ry;
-		socket.userData.rz = data.rz;
-		socket.userData.rw = data.rw;
-	});
-});
+sockets(io);
 
 // called 20 times a second on the server side
 setInterval(function () {
